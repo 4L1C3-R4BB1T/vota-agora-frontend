9e27c5c69cb430fefc77a53efa1f13ad18663b8a
gabiroba <gabrielcardosogirarde@gmail.com>
2024-09-30 19:02:24 -0300

    refactor: Ajuste no header

--- src/core/components/Header/index.tsx
+++ src/core/components/Header/index.tsx
@@ -3,11 +3,20 @@ import { Avatar, AvatarImage } from '@/components/ui/avatar';
 import logo from '../../../assets/logo.png';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu"
 
+  
 function Header() {
     return (
         <header>
-            <div className="flex items-center justify-between py-3  ps-20 pe-10">
+            <div className="flex items-center justify-between py-3  p-20 ">
                 <div className="flex gap-2 items-center">
                     <img src={logo} className="w-[200px]" />
                 </div>
@@ -22,9 +31,19 @@ function Header() {
                         <AvatarImage src="https://github.com/shadcn.png" />
                     </Avatar>
                     <div>
-                        <Button className="bg-brand-primary hover:bg-brand-primary">
-                            <i className="fa-solid fa-bars"></i>
-                        </Button>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger className="outline-none">
+                                <Button className="bg-brand-primary hover:bg-brand-primary">
+                                    <i className="fa-solid fa-bars"></i>
+                                </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent>
+                                <DropdownMenuLabel>Minha Conta</DropdownMenuLabel>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem>Perfil</DropdownMenuItem>
+                                <DropdownMenuItem>Sair</DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
                     </div>
                 </div>
             </div>
