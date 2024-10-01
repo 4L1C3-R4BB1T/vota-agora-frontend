f8ae1f64e8857780c3dc779c2dd61d071e265bfc
gabiroba <gabrielcardosogirarde@gmail.com>
2024-10-01 09:03:13 -0300

    feat: Carousel

--- src/core/components/HighlightPublicConsultation/index.tsx
+++ src/core/components/HighlightPublicConsultation/index.tsx
@@ -1,12 +1,39 @@
 import HighlightPublicConsultationItem from "../HighlightPublicConsultationItem";
-
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+  } from "@/components/ui/carousel"
+  
 function HighlightPublicConsultation() {
     return (
-        <div className="flex gap-4">
-            <HighlightPublicConsultationItem/>
-            <HighlightPublicConsultationItem/>
-            <HighlightPublicConsultationItem/>
-            <HighlightPublicConsultationItem/>
+        <div>
+            <Carousel>
+                <CarouselContent>
+                    <CarouselItem className="basis-1/4">
+                        <HighlightPublicConsultationItem/>
+                    </CarouselItem>
+                    <CarouselItem className="basis-1/4">
+                        <HighlightPublicConsultationItem/>
+                    </CarouselItem>
+                    <CarouselItem className="basis-1/4">
+                        <HighlightPublicConsultationItem/>
+                    </CarouselItem>
+                    <CarouselItem className="basis-1/4">
+                        <HighlightPublicConsultationItem/>
+                    </CarouselItem>
+                    <CarouselItem className="basis-1/4">
+                        <HighlightPublicConsultationItem/>
+                    </CarouselItem>
+                    <CarouselItem className="basis-1/4">
+                        <HighlightPublicConsultationItem/>
+                    </CarouselItem>
+                </CarouselContent>
+                <CarouselPrevious />
+                <CarouselNext />
+            </Carousel>
         </div>
     );
 }
